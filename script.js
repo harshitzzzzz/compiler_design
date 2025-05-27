8609f72eab9c3ff6bd249957bc0f9a54aedb1268
Shobha <bartishobha00@gmail.com>
2025-05-27 21:27:31 +0530

    modified styles

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     document.getElementById('runButton').addEventListener('click', compileAndRun);
-
+//lexing
     function lexer(input) {
         const tokens = [];
         let position = 0;
